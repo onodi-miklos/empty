535d35f595f9b809fb2bb6482d6c89656e1f7802
Ónodi Miklós <mikonodi33@gmail.com>
2026-07-22 18:50:56 +0200

    Update dependencies and configuration files
    - Added @types/express and @types/webpack-env to devDependencies in package.json.
    - Updated TypeScript version to 5.3.3 in package.json.
    - Changed moduleResolution from 'bundler' to 'node' in tsconfig.json.
    - Added support for importing CSS modules in shims-vue.d.ts.
    - Removed TypeScript ignore comment for global CSS import in main.ts.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// @ts-ignore: Allow importing global CSS in this TypeScript entry file
 import './assets/global.css'
 
 createApp(App).use(router).mount('#app')
