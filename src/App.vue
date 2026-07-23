da96d52d936dd4af77b6268528aa64ef92f8a85c
Ónodi Miklós <mikonodi33@gmail.com>
2026-07-23 09:34:14 +0200

    Fixed issues

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  
+  <div></div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
 
 <style>
 
